4bd8330029638e97029bb8da99fb473d2af303d1
MinhausKILL <warlordsxp@yandex.ru>
2023-12-14 20:48:30 +0300

    Add easygui add all function

--- Lesson8.py
+++ Lesson8.py
@@ -1,22 +1,71 @@
 import sqlite3
- 
-conn = sqlite3.connect("phonebook.db") # или :memory: чтобы сохранить в RAM
+import easygui
+
+# Создаем подключение к базе данных
+conn = sqlite3.connect('phonebook.db')
 cursor = conn.cursor()
- 
-# Создание таблицы
-cursor.execute("""CREATE TABLE IF NOT EXISTS users
-                  (id INTEGER PRIMARY KEY, 
-               name TEXT, 
-               surname TEXT,
-               number INTEGER)
-               """)
+
+# Создаем таблицу, если её нет
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS contacts (
+        id INTEGER PRIMARY KEY,
+        name TEXT NOT NULL,
+        number TEXT NOT NULL
+    )
+''')
 conn.commit()
 
-# Вставляем множество данных в таблицу используя безопасный метод "?"
-users = [('1', 'Maria', 'Sucheva', '89230568741', ),
-          ('2', 'Pavel', 'Malyk', '89645507777', ),
-          ('3', 'Mihail', 'Kristoforov', '89547502358', ),
-          ('4', 'Andrey', 'Karpushenkov', '89065584587', )]
- 
-cursor.executemany("INSERT INTO users VALUES (?,?,?,?)", users)
-conn.commit()
+def add_contact(name, number):
+    cursor.execute('INSERT INTO contacts (name, number) VALUES (?, ?)', (name, number))
+    conn.commit()
+    easygui.msgbox(f"Контакт '{name}' добавлен успешно.", "Успех")
+
+def view_contacts():
+    cursor.execute('SELECT * FROM contacts')
+    contacts = cursor.fetchall()
+    if contacts:
+        for contact in contacts:
+            print(f"{contact[0]}. {contact[1]}: {contact[2]}")
+    else:
+        print("Телефонный справочник пуст.")
+
+def search_contact(name):
+    cursor.execute('SELECT * FROM contacts WHERE name LIKE ?', (f'%{name}%',))
+    result = cursor.fetchall()
+    if result:
+        for contact in result:
+            print(f"{contact[0]}. {contact[1]}: {contact[2]}")
+    else:
+        print(f"Контакт с именем '{name}' не найден.")
+
+def delete_contact(contact_id):
+    cursor.execute('DELETE FROM contacts WHERE id = ?', (contact_id,))
+    conn.commit()
+    print("Контакт удален успешно.")
+
+def update_contact(contact_id, new_number):
+    cursor.execute('UPDATE contacts SET number = ? WHERE id = ?', (new_number, contact_id))
+    conn.commit()
+    print("Контакт обновлен успешно.")
+
+while True:
+    choice = easygui.buttonbox("Выберите действие:", choices=["Просмотреть контакты", "Добавить контакт", "Искать контакт", "Удалить контакт", "Обновить контакт", "Выйти"])
+
+    if choice == "Просмотреть контакты":
+        view_contacts()
+    elif choice == "Добавить контакт":
+        name = easygui.enterbox("Введите имя контакта:")
+        number = easygui.enterbox("Введите номер телефона:")
+        add_contact(name, number)
+    elif choice == "Искать контакт":
+        name = easygui.enterbox("Введите имя контакта для поиска:")
+        search_contact(name)
+    elif choice == "Удалить контакт":
+        contact_id = easygui.enterbox("Введите ID контакта для удаления:")
+        delete_contact(contact_id)
+    elif choice == "Обновить контакт":
+        contact_id = easygui.enterbox("Введите ID контакта для обновления:")
+        new_number = easygui.enterbox("Введите новый номер телефона:")
+        update_contact(contact_id, new_number)
+    elif choice == "Выйти":
+        break
